7cb4d263f9aaf18855585a88431ca14f5d8cd114
mmihye <smilehye910@naver.com>
2023-11-23 02:20:31 +0900

    [#4] feat: repository 추가

---
+++ skyscanner/src/main/java/org/third/thirdseminar/infrastructure/ReservationJpaRepository.java
@@ -0,0 +1,9 @@
+package org.third.thirdseminar.infrastructure;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.third.thirdseminar.domain.Reservation;
+
+import java.util.List;
+
+public interface ReservationJpaRepository extends JpaRepository<Reservation,Long> {
+}
